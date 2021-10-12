61555ac00bb1c62900018085ef2e642bb2bf273d
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-10-12 13:43:08 +0200

    Create ShoppingCart.java and ShoppingCartServlet and Set Up html Crads Div ID

---
+++ src/main/java/com/codecool/shop/controller/ShoppingCartServlet.java
@@ -0,0 +1,15 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@WebServlet(urlPatterns = {"/shopping-cart"})
+public class ShoppingCartServlet extends HttpServlet {
+}
