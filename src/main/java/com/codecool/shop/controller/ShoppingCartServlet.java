f81adcb0bb0ce74675c7b22974d6a2b083924b2b
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-10-13 13:50:14 +0200

    [#5] Save Cart content to Json

--- src/main/java/com/codecool/shop/controller/ShoppingCartServlet.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartServlet.java
@@ -2,7 +2,9 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.HttpRequestJava;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
 //import com.google.gson.reflect.TypeToken;
@@ -21,6 +23,7 @@ import java.util.List;
 @WebServlet(name = "ShoppingCartServlet", urlPatterns = "/shoppingcart")
 public class ShoppingCartServlet extends HttpServlet {
     ProductDao productDao = ProductDaoMem.getInstance();
+    ShoppingCartDao shoppingCartDataStore = ShoppingCartDaoMem.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,13 +31,16 @@ public class ShoppingCartServlet extends HttpServlet {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-
         String responsId = request.getParameter("id");
         int id = Integer.parseInt(responsId);
 
         Product product = productDao.find(id);
+
+        // add Mem
+        shoppingCartDataStore.add(product);
+
         Gson gson = new Gson();
-        System.out.println(gson.toJson(product));
+        System.out.println(gson.toJson(shoppingCartDataStore.getAll()));
         PrintWriter out = response.getWriter();
         out.println(gson.toJson(product));
     }
