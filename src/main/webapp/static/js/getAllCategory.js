ac50455be49ef55842554173c31aeb7fb4b785ab
l-d-b <laszlo.dora.beata@gmail.com>
2021-10-13 14:43:01 +0200

    [#19] add JS for get categories

---
+++ src/main/webapp/static/js/getAllCategory.js
@@ -0,0 +1,18 @@
+function init() {
+    fetch(`/api/get_all_category`)
+        .then(response => response.json())
+        .then(data => {
+            cardFactory(data);
+        })
+}
+
+function cardFactory(data) {
+    const forCards = document.querySelector("#products");
+    forCards.innerHTML = "";
+    data.forEach(item => {
+        forCards.innerHTML += htmlFactory(item);
+        console.log(item);
+    })
+}
+
+init()
