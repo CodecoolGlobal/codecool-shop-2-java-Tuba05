8b83fe27b396f093d42b2975c07964555c67da0b
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-10-13 11:57:34 +0200

    Delete Button delete product from cart in frontend

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -2,6 +2,7 @@ import { putProductToShoppingCart } from './shoppingCart.js';
 
 function main(){
     putProductToShoppingCart()
+
 }
 
 main();
--- src/main/webapp/static/js/shoppingCart.js
+++ src/main/webapp/static/js/shoppingCart.js
@@ -6,6 +6,7 @@ export function putProductToShoppingCart() {
         successButton.addEventListener("click", () => {
             let url = `/shoppingcart?id=${successButton.id}`;
             productFetch(url);
+
         })
     }
 }
@@ -31,11 +32,12 @@ function shoppingCartContentCreator(data){
                 <input type="number" class="form-control text-center" value="1">
             </td>
             <td>12,000</td>
-            <td class="actions" data-th="" style="width:10%;">
-                <button class="btn btn-warning btn-sm"><i class="fa fa-refresh"></i></button>
-                <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+            <td id="${data.id}" class="actions" data-th="" style="width:10%;">
+                <button class="btn btn-warning btn-sm" type="submit"><i class="fa fa-refresh"></i></button>
+                <button id="delete-button" class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-o"></i></button>
             </td>
         </tr>`
+    deleteProductFromCart();
 }
 
 function productFetch(url){
@@ -50,4 +52,17 @@ function productInCartCounter(number){
     // counter.parentElement.style.background = rgb();
     let currentNumber = parseInt(counter.innerText);
     counter.innerHTML = (currentNumber + number);
+
+}
+
+export function deleteProductFromCart(){
+    let deleteButtons = document.querySelectorAll("#delete-button")
+    for (let deleteButton of deleteButtons) {
+        deleteButton.addEventListener("click", () => {
+            console.log(deleteButton.parentElement.id)
+            deleteButton.parentElement.parentElement.remove();
+            // let url = `/shoppingcart?id=${successButton.id}`;
+            // productFetch(url);
+        })
+    }
 }
