bfd4a61de3aa45fabb0d7e0438e52610c6032f91
Tuba05 <bbence925@gmail.com>
2021-10-14 16:39:46 +0200

    [#28] add event listener for department in navbar

--- src/main/webapp/static/js/eventListenersForNavbar.js
+++ src/main/webapp/static/js/eventListenersForNavbar.js
@@ -1,9 +1,16 @@
 import {getAllProductsByCategory} from "./getAllProductByCategory.js";
+import {getAllCategory} from "./getAllCategoryByDepartment.js";
 
+const departments = document.querySelectorAll(".department-button");
+departments.forEach(department => {
+    department.addEventListener('click', ()=> {
+        getAllCategory(department.dataset.name)});
+})
 
 const categories = document.querySelectorAll(".category-button");
 categories.forEach(category => {
     category.addEventListener('click', () => {
+        console.log(category.innerHTML)
         getAllProductsByCategory(category.innerHTML);
     })
 })
