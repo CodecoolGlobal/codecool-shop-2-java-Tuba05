9b7c1519bd89b000c4ddc0e7ce5373589d23b7d3
Tuba05 <bbence925@gmail.com>
2021-10-14 09:23:22 +0200

    [#20] add getBy method with String parameter in ProductDaoMem

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
@@ -57,4 +57,9 @@ public class ProductDaoMem implements ProductDao {
     public List<Product> getBy(ProductCategory productCategory) {
         return data.stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
     }
+
+    @Override
+    public List<Product> getBy(String productCategory) {
+        return data.stream().filter(t -> t.getProductCategory().getName().equals(productCategory)).collect(Collectors.toList());
+    }
 }
