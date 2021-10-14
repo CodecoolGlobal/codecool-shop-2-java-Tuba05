77a8860e032bdcc4016ad7e1cfe782e342f20833
Tuba05 <bbence925@gmail.com>
2021-10-14 13:54:39 +0200

    [#28] fix getAllProductByCategory.js

--- src/main/webapp/static/js/getAllProductByCategory.js
+++ src/main/webapp/static/js/getAllProductByCategory.js
@@ -1,7 +1,7 @@
 import {cardFactory} from "./productFactory.js";
 
-function init() {
-    fetch(`/api/get_all_product_by_category?category=Glasses`)
+export function init(category) {
+    fetch(`/api/get_all_product_by_category?category=${category}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
