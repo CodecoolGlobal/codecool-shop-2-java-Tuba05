f3dba5f84e5f78ff3d95ac029a6d9123606357ea
Tuba05 <bbence925@gmail.com>
2021-10-14 13:27:25 +0200

    [#28] add list of buttons

---
+++ src/main/webapp/static/js/eventListenersForNavbar.js
@@ -0,0 +1,15 @@
+const buttons = {
+    clothes : document.querySelector("#cloths-button"),
+    coats : document.querySelector("#coats-button"),
+    shirts : document.querySelector("#shirts-button"),
+    denim : document.querySelector("#cloths-button"),
+    shoes : document.querySelector("#shoes-button"),
+    heels : document.querySelector("#heels-button"),
+    sneakers : document.querySelector("#sneakers-button"),
+    boots : document.querySelector("#boots-button"),
+    accessories : document.querySelector("#accessories-button"),
+    glasses : document.querySelector("#glasses-button"),
+    jewelery : document.querySelector("#jewelry-button"),
+    other : document.querySelector("#other")
+}
+
