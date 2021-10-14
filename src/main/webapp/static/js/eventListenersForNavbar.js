cc98ffb15b81769490b1842e1ce04b9de0df2f15
l-d-b <laszlo.dora.beata@gmail.com>
2021-10-14 16:44:30 +0200

    [#28] add event listener to Home button

--- src/main/webapp/static/js/eventListenersForNavbar.js
+++ src/main/webapp/static/js/eventListenersForNavbar.js
@@ -1,7 +1,15 @@
 import {getAllProductsByCategory} from "./getAllProductByCategory.js";
+import {init} from "./getAllDepartment.js";
 
 
 const categories = document.querySelectorAll(".category-button");
+const homeButton = document.querySelector(".home-button");
+
+homeButton.addEventListener('click', event => {
+    init();
+    }
+)
+
 categories.forEach(category => {
     category.addEventListener('click', () => {
         getAllProductsByCategory(category.innerHTML);
