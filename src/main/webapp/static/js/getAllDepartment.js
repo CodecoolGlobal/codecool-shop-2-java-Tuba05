06b67e5c9961513fd454a9e767a37186e160e29c
Tuba05 <bbence925@gmail.com>
2021-11-10 14:39:32 +0100

    [#12] replace htmlFactory function to HtmlFactory class

--- src/main/webapp/static/js/getAllDepartment.js
+++ src/main/webapp/static/js/getAllDepartment.js
@@ -1,4 +1,6 @@
 import {getAllCategory} from "/static/js/getAllCategoryByDepartment.js";
+import HtmlFactory from "./HtmlFactory.js";
+
 const forCards = document.querySelector("#products");
 
 export const getAllDepartment = {
@@ -13,34 +15,17 @@ export const getAllDepartment = {
 
     cardFactory: function (data){
         forCards.innerHTML = "";
-        data.forEach(item => {
-            forCards.innerHTML += this.htmlFactory(item);
+        data.forEach(department => {
+            const html = new HtmlFactory(department, "See all").createHtml();
+            forCards.innerHTML += html;
         })
     },
 
-    htmlFactory: function (item){
-        return `
-        <div class="col col-sm-12 col-md-6 col-lg-4">
-            <div class="card" id="${item.name}">
-                <img class="" style="width:100%;height:300px;" src="/static/img/Department/${item.name}.jpg"/>
-                <div class="card-header">
-                    <h4 class="card-title">${item.name}</h4>
-                </div>
-                <div class="card-body">
-                    <div class="card-btn">
-                        <a class="btn btn-success" id="see-all" data-department="${item.name}" style="background: #ffd900; color: #333333; border-color: #ffd900"
-                        >See all</a>
-                    </div>
-                </div>
-            </div>
-            </div>
-    `
-    },
+
     addEventListenerToButton: function (){
         const seeAllButtons = document.querySelectorAll("#see-all");
         seeAllButtons.forEach(button => {
             button.addEventListener('click', () => {
-                console.log("clicked");
                 let department = button.dataset.department;
                 getAllCategory(department)
             })
