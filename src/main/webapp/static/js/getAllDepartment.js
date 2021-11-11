6470a256b5476f7e304ed5dd79d807dde02d85bf
Tuba05 <bbence925@gmail.com>
2021-11-11 14:02:18 +0100

    [#12] replace getAllDepartment.js

--- src/main/webapp/static/js/getAllDepartment.js
+++ src/main/webapp/static/js/getAllDepartment.js
@@ -1,35 +1,20 @@
-import {getAllCategory} from "/static/js/getAllCategoryByDepartment.js";
-import HtmlFactory from "./HtmlFactory.js";
+import {cardFactory} from "./cardFactory.js";
+import {addEventListenerToButton} from "./addEventListenerToButton.js";
 
-const forCards = document.querySelector("#products");
+const querySelectors = {
+    forCards: document.querySelector("#products"),
+    seeAllButtons: document.querySelectorAll("#see-all")
+}
 
 export const getAllDepartment = {
     init: function () {
         fetch(`/api/get_all_department`)
             .then(response => response.json())
             .then(data => {
-                this.cardFactory(data);
-                setTimeout(this.addEventListenerToButton, 200);
+                cardFactory.init(querySelectors.forCards, data, "See all", "department");
+                const seeAllButtons = document.querySelectorAll("#see-all")
+                addEventListenerToButton(seeAllButtons, "department");
             })
     },
-
-    cardFactory: function (data){
-        forCards.innerHTML = "";
-        data.forEach(department => {
-            const html = new HtmlFactory(department, "See all").createHtml();
-            forCards.innerHTML += html;
-        })
-    },
-
-
-    addEventListenerToButton: function (){
-        const seeAllButtons = document.querySelectorAll("#see-all");
-        seeAllButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                let department = button.dataset.department;
-                getAllCategory(department)
-            })
-        })
-    }
 }
 
