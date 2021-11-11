61e44cd177fdd94f915ae9fc1e24d72cd707e2fa
l-d-b <laszlo.dora.beata@gmail.com>
2021-11-11 20:13:43 +0100

    [#1] add test login_with_invalid_username_and_password_return_false()

--- src/test/java/com/codecool/shop/LoginTest.java
+++ src/test/java/com/codecool/shop/LoginTest.java
@@ -4,6 +4,7 @@ import com.codecool.shop.controller.LoginServlet;
 import org.apache.struts.mock.MockHttpServletRequest;
 import org.apache.struts.mock.MockHttpServletResponse;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.mockito.ArgumentCaptor;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Objects;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -32,10 +34,23 @@ public class LoginTest {
         Mockito.when(request.getSession()).thenReturn(session);
         LoginServlet loginServlet = new LoginServlet();
         loginServlet.doPost(request, response);
-        System.out.println(response.getHeaderNames());
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         verify(response).sendRedirect(captor.capture());
-        System.out.println(captor.getValue());
         Assertions.assertEquals("home", captor.getValue());
+    }
+
+    @Test
+    void login_with_invalid_username_and_password_return_false() throws ServletException, IOException {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        Mockito.when(request.getParameter("uname")).thenReturn("Invalid name");
+        Mockito.when(request.getParameter("psw")).thenReturn("123");
+        HttpSession session = Mockito.mock(HttpSession.class);
+        Mockito.when(request.getSession()).thenReturn(session);
+        LoginServlet loginServlet = new LoginServlet();
+        loginServlet.doPost(request, response);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(response).sendRedirect(captor.capture());
+        Assertions.assertNotEquals("home", captor.getValue());
     }
 }
