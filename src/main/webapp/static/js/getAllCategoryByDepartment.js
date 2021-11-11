076b87c9e27e91a8af1c9b303327db649d9560cc
Tuba05 <bbence925@gmail.com>
2021-11-11 11:44:25 +0100

    [#12] refactor getAllCategoryByDepartment.js

--- src/main/webapp/static/js/getAllCategoryByDepartment.js
+++ src/main/webapp/static/js/getAllCategoryByDepartment.js
@@ -1,55 +1,36 @@
 import {getAllProductsByCategory} from "./getAllProductByCategory.js";
+import HtmlFactory from "./HtmlFactory.js";
+import {utils} from "./utils.js";
 
 const forCards = document.querySelector("#products");
 
-export function getAllCategory(department) {
-    fetch(`/api/get_all_category?department=${department}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            cardFactory(data);
-            addEventListenerToButton();
+export const init = {
+    getAllCategory : function (department){
+        fetch(`/api/get_all_category?department=${department}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.cardFactory(data);
+                utils.setTimeOutToAddEventListener(this.addEventListenerToButton);
+            })
+    },
+
+    cardFactory: function (data){
+        utils.clearHtml(forCards);
+        data.forEach(item => {
+            const html = new HtmlFactory(item, "See products").createHtmlForCategory();
+            forCards.innerHTML += html;
         })
-}
+    },
 
-function cardFactory(data) {
-    forCards.innerHTML = "";
-    data.forEach(item => {
-        forCards.innerHTML += htmlFactory(item);
-    })
-}
-
-function htmlFactory(item) {
-    console.log(item.name);
-    return `
-        <div class="col col-sm-12 col-md-6 col-lg-4">
-            <div class="card" id="${item.name}">
-                <img class="" style="width:100%;height:300px;" src="/static/img/${item.name}.jpg"/>
-                <div class="card-header">
-                    <h4 class="card-title">${item.name}</h4>
-                </div>
-                <div class="card-body">
-                    <div class="card-btn">
-                        <a class="btn btn-success see-all-product" data-category="${item.name}" style="background: #ffd900; color: #333333; border-color: #ffd900" >See products</a>
-                    </div>
-                </div>
-            </div>
-            </div>`
-
-}
-
-function addEventListenerToButton() {
-    const seeAllButtons = document.querySelectorAll(".see-all-product");
-    console.log(seeAllButtons);
-
-    seeAllButtons.forEach(button => {
-        button.addEventListener('click', event => {
-            console.log("clicked");
-            let category = button.dataset.category;
-            console.log(button);
-            console.log(category)
-            getAllProductsByCategory(category)
+    addEventListenerToButton: function (){
+        const seeAllButtons = document.querySelectorAll(".see-all-product");
+        seeAllButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                let category = button.dataset.category;
+                getAllProductsByCategory(category)
+            })
         })
-    })
+    }
 }
 
